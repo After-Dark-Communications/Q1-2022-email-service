2bc90423a4baf04a22841d2d82d29dfac10d010f
Luc Somers <lucsomers@hotmail.com>
2022-05-16 10:11:54 +0200

    Create EmailSettingsController.cs

---
+++ EmailService/Controllers/EmailSettingsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmailService.Controllers
+{
+    public class EmailSettingsController : Controller
+    {
+        [Route("UpdateEmailSettings")]
+        [HttpPost]
+        public IActionResult UpdateEmailSettings()
+        {
+            try
+            {
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                BadRequest(e.Message);
+            }
+        }
+
+    }
+}
