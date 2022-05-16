16ee3ca4d869296477bdd24fbf94504afd3e2776
Luc Somers <lucsomers@hotmail.com>
2022-05-16 13:35:33 +0200

    Revert "Added first setup for file saver"
    This reverts commit 1f7d767c87db0054648c80283de96af46e259f2d.

--- EmailService/Controllers/EmailSettingsController.cs
+++ EmailService/Controllers/EmailSettingsController.cs
@@ -1,50 +1,22 @@
-﻿using EmailService.Models;
-using EmailService.Services.IServices;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace EmailService.Controllers
 {
-    [ApiController]
-    [Route("[Controller]")]
     public class EmailSettingsController : Controller
     {
-        private IEmailSettingsService emailSettingsService;
-
-        public EmailSettingsController(IEmailSettingsService emailSettingsService)
-        {
-            this.emailSettingsService = emailSettingsService;
-        }
-
         [Route("UpdateEmailSettings")]
         [HttpPost]
-        public IActionResult UpdateEmailSettings(EmailSettings emailSettings)
+        public IActionResult UpdateEmailSettings()
         {
             try
             {
-                emailSettingsService.UpdateEmailSettings(emailSettings);
+                
+                return Ok();
             }
             catch (Exception e)
             {
                 BadRequest(e.Message);
             }
-
-            return Ok();
-        }
-
-        public IActionResult GetDefaultEmailSettings()
-        {
-            EmailSettings? defaultEmailSettings = null;
-
-            try
-            {
-                defaultEmailSettings = emailSettingsService.GetDefaultEmailSettings();
-            }
-            catch (Exception e)
-            {
-                BadRequest(e.Message);
-            }
-
-            return Ok(defaultEmailSettings);
         }
 
     }
--- EmailService/Services/EmailSettingsService.cs
+++ EmailService/Services/EmailSettingsService.cs
@@ -1,23 +1,6 @@
-﻿using EmailService.Factories;
-using EmailService.Models;
-
-namespace EmailService.Services.IServices
+﻿namespace EmailService.Services.IServices
 {
-    public class EmailSettingsService : IEmailSettingsService
+    public class EmailSettingsService
     {
-        public EmailSettings GetDefaultEmailSettings()
-        {
-            EmailSettings settings = EmailSettingsFactory.GetDefaultEmailSettings();
-
-            if (settings == null)
-                throw new Exception("Settings cannot be created something went wrong");
-
-            return settings;
-        }
-
-        public void UpdateEmailSettings(EmailSettings emailSettings)
-        {
-            //TODO: save email settings
-        }
     }
 }
