09099f98cc5be0d71dbb3c27fe3242c4b0ce5901
Luc Somers <lucsomers@hotmail.com>
2022-05-19 15:06:53 +0200

    Added mongodb connection

---
+++ EmailService/Context/EmailSettingsContext.cs
@@ -0,0 +1,37 @@
+﻿using EmailService.Context.IContexts;
+using EmailService.Models;
+using MongoDB.Driver;
+
+namespace EmailService.SaveSystem
+{
+    public class EmailSettingsContext : IEmailSettingsContext
+    {
+        private const string EmailSettingsDatabaseKey = "";
+        private MongoClient client;
+
+        public void Connect()
+        {
+            client = new MongoClient("mongodb+srv://Hoeleboele:<password>@cluster0.mshnd.mongodb.net/email-settings");
+
+            IMongoDatabase database = client.GetDatabase("email-settings");
+
+            database.CreateCollection("email-settings");
+
+            IMongoCollection<EmailSettings> mongoCollection = database.GetCollection<EmailSettings>("email-settings");
+
+            EmailSettings emailsetting = mongoCollection.Find(e => e.Footer == "").FirstOrDefault();
+        }
+
+        public void SaveEmailSettings()
+        {
+
+        }
+
+        public EmailSettings GetEmailSettingsById(int id)
+        {
+            EmailSettings emailSettings = null;
+
+            return emailSettings;
+        }
+    }
+}
---
+++ EmailService/Context/IContexts/IEmailSettingsContext.cs
@@ -0,0 +1,11 @@
+﻿using EmailService.Models;
+
+namespace EmailService.Context.IContexts
+{
+    public interface IEmailSettingsContext
+    {
+        public void SaveEmailSettings();
+
+        public EmailSettings GetEmailSettingsById(int id);
+    }
+}
