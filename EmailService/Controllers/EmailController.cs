6115b62681ba1a7fdf73a12d0c47a08e2ccb3970
Jop <jop.huijbers2001@gmail.com>
2022-06-13 12:13:03 +0200

    remove email controller endpoints

--- EmailService/Controllers/EmailController.cs
+++ EmailService/Controllers/EmailController.cs
@@ -10,83 +10,83 @@ namespace EmailService.Controllers
     [Route("[Controller]")]
     public class EmailController : Controller
     {
-        private readonly IOptions<Security> security;
+        //private readonly IOptions<Security> security;
 
-        private readonly ISendEmailService emailService;
-        private readonly string wwwRoot;
-        public EmailController(ISendEmailService emailService, IOptions<Security> security, IWebHostEnvironment environment)
-        {
-            this.emailService = emailService;
-            this.security = security;
-            wwwRoot = environment.WebRootPath;
-        }
+        //private readonly ISendEmailService emailService;
+        //private readonly string wwwRoot;
+        //public EmailController(ISendEmailService emailService, IOptions<Security> security, IWebHostEnvironment environment)
+        //{
+        //    this.emailService = emailService;
+        //    this.security = security;
+        //    wwwRoot = environment.WebRootPath;
+        //}
 
-        [Route("Send")]
-        [HttpPost]
-        public IActionResult SendEmail([FromBody] UserEmail email)
-        {
-            //foreach (UserEmail email in emails)
-            //{
-            try
-            {
-                EmailInfo emailInfo = new EmailInfo(security)
-                {
-                    BodyFormat = new string[] { "test body format", "testje", "teste" },
-                    //ReceiverAddress = email.Email,
-                    ReceiverAddress = "lucsomers@hotmail.com",
-                    Subject = "Survey DinnerInMotion",
-                    TemplateFilePath = Path.Combine(wwwRoot, "Templates/DimMail.htm"),
-                    Username = "Luc"
-                    //Username = email.Username
-                };
+        //[Route("Send")]
+        //[HttpPost]
+        //public IActionResult SendEmail([FromBody] UserEmail email)
+        //{
+        //    //foreach (UserEmail email in emails)
+        //    //{
+        //    try
+        //    {
+        //        EmailInfo emailInfo = new EmailInfo(security)
+        //        {
+        //            BodyFormat = new string[] { "test body format", "testje", "teste" },
+        //            //ReceiverAddress = email.Email,
+        //            ReceiverAddress = "lucsomers@hotmail.com",
+        //            Subject = "Survey DinnerInMotion",
+        //            TemplateFilePath = Path.Combine(wwwRoot, "Templates/DimMail.htm"),
+        //            Username = "Luc"
+        //            //Username = email.Username
+        //        };
 
-                emailService.SendEmail(emailInfo);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-            //}
+        //        emailService.SendEmail(emailInfo);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return BadRequest(e.Message);
+        //    }
+        //    //}
 
 
-            return Ok();
-        }
+        //    return Ok();
+        //}
 
-        [Route("Preview")]
-        [HttpGet]
-        public IActionResult GetMailPreview()
-        {
-            try
-            {
-                EmailInfo emailInfo = new EmailInfo(security)
-                {
-                    TemplateFilePath = Path.Combine(wwwRoot, "Templates/DimMail.htm")
-                };
-                return base.Content(emailInfo.GetHTMLTemplate("klant", "https://www.dupuis.com/v5/img/visuels_resume/LL.jpg"), "text/html");
-            }
-            catch (Exception e)
-            {
+        //[Route("Preview")]
+        //[HttpGet]
+        //public IActionResult GetMailPreview()
+        //{
+        //    try
+        //    {
+        //        EmailInfo emailInfo = new EmailInfo(security)
+        //        {
+        //            TemplateFilePath = Path.Combine(wwwRoot, "Templates/DimMail.htm")
+        //        };
+        //        return base.Content(emailInfo.GetHTMLTemplate("klant", "https://www.dupuis.com/v5/img/visuels_resume/LL.jpg"), "text/html");
+        //    }
+        //    catch (Exception e)
+        //    {
 
-                return BadRequest(e.Message);
-            }
-        }
-        [Route("Preview/{path}")]
-        [HttpGet]
-        public IActionResult GetMailPreview(string path)
-        {
-            try
-            {
-                EmailInfo emailInfo = new EmailInfo(security)
-                {
-                    TemplateFilePath = Path.Combine(wwwRoot, path)
-                };
-                return base.Content(emailInfo.GetHTMLTemplate("klant", "https://www.dupuis.com/v5/img/visuels_resume/LL.jpg"), "text/html");
-            }
-            catch (Exception e)
-            {
+        //        return BadRequest(e.Message);
+        //    }
+        //}
+        //[Route("Preview/{path}")]
+        //[HttpGet]
+        //public IActionResult GetMailPreview(string path)
+        //{
+        //    try
+        //    {
+        //        EmailInfo emailInfo = new EmailInfo(security)
+        //        {
+        //            TemplateFilePath = Path.Combine(wwwRoot, path)
+        //        };
+        //        return base.Content(emailInfo.GetHTMLTemplate("klant", "https://www.dupuis.com/v5/img/visuels_resume/LL.jpg"), "text/html");
+        //    }
+        //    catch (Exception e)
+        //    {
 
-                return BadRequest(e.Message);
-            }
-        }
+        //        return BadRequest(e.Message);
+        //    }
+        //}
     }
 }
