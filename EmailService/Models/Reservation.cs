0ef3a24e5d70de17294094c8cddcb1ce8c55dd6c
Jop <jop.huijbers2001@gmail.com>
2022-06-13 12:12:48 +0200

    send email after receive message from service bus

--- EmailService/Models/Reservation.cs
+++ EmailService/Models/Reservation.cs
@@ -4,6 +4,7 @@
     public class Reservation
     {
         public int id { get; set; }
+        public string? name { get; set; }
         public string? email { get; set; }
         public string? date { get; set; }
     }
